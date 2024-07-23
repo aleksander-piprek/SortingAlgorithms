7becec4d69ddfc76360cc26075ff4ec7cc0a4103
Aleksander Piprek <kerpipkelo@gmail.com>
2024-07-23 17:19:51 +0200

    Added more functions to Blocking Queue

--- commons/queue/blocking_queue.cpp
+++ commons/queue/blocking_queue.cpp
@@ -1,13 +1,55 @@
 #include "blocking_queue.hpp"
 
-T BlockingQueue::pop()
+template<class T>
+void BlockingQueue<T>::push(T& data)
 {
-    std::lock_quard<std::mutex> lock(this->mutex_);
-    T value;
-    return value;
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_.push(data);
 }
 
-void BlockingQueue::push(T value)
+template<class T>
+bool BlockingQueue<T>::empty() const
 {
-    std::lock_guard<std::mutex> lock(this->mutex_);
+    std::lock_guard<std::mutex> lock(mutex_);
+    return queue_.empty();
+}
+
+template<class T>
+bool BlockingQueue<T>::tryPop(T& value)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    if(queue_.empty())
+        return false;
+
+    value = queue_.front();
+    queue_.pop();
+    return true;
+}
+
+template<class T>
+void BlockingQueue<T>::waitAndPop(T& _value)
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (queue_.empty())
+    {
+        signal_.wait(lock);
+    }
+
+    _value = queue_.front();
+    queue_.pop();
+}
+
+template<class T>
+bool BlockingQueue<T>::tryWaitAndPop(T& _value, int _time)
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (queue_.empty())
+    {
+        signal_.wait_for(lock, std::chrono::milliseconds(_time));
+        return false;
+    }
+
+    _value = queue_.front();
+    queue_.pop();
+    return true;
 }
--- commons/queue/blocking_queue.hpp
+++ commons/queue/blocking_queue.hpp
@@ -5,16 +5,20 @@
 #include <queue>
 #include <mutex>
 #include <thread>
+#include <condition_variable>
 
 template <typename T>
 class BlockingQueue
 {
-    std::mutex mutex_;
+    mutable std::mutex mutex_;
     std::queue<T> queue_;
+    std::condition_variable signal_;
     public:
-        T pop();
-        void push(T value);
-    
+        void push(T& data);     
+        bool empty() const;            
+        bool tryPop(T& value);
+        void waitAndPop(T& _value);
+        bool tryWaitAndPop(T& _value, int _time);
 };
 
 #endif //BLOCKING_QUEUE_HPP
